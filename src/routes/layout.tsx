6549d3c1d67e7888e0d48d775405fd51f7679433
gioboa <giorgiob.boa@gmail.com>
2024-04-21 15:33:26 +0200

    feat: change layout

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,5 +1,7 @@
 import { component$, Slot } from "@builder.io/qwik";
 import type { RequestHandler } from "@builder.io/qwik-city";
+import { Footer } from "~/components/Footer";
+import { Nav } from "~/components/Nav";
 
 export const onGet: RequestHandler = async ({ cacheControl }) => {
   // Control caching for this request for best performance and to reduce hosting costs:
@@ -13,5 +15,13 @@ export const onGet: RequestHandler = async ({ cacheControl }) => {
 };
 
 export default component$(() => {
-  return <Slot />;
+  return (
+    <div class="font-roboto bg-gray-100">
+      <Nav />
+      <main>
+        <Slot />
+      </main>
+      <Footer />
+    </div>
+  );
 });
