80ba83af5cf5b73a95b0efa0612829bddf07edc2
gioboa <giorgiob.boa@gmail.com>
2024-04-22 22:53:31 +0200

    feat: add gql for posts

---
+++ src/queries/posts.ts
@@ -0,0 +1,348 @@
+import gql from "graphql-tag";
+import { graphqlSdk } from "~/utils/graphql-wrapper";
+
+export const allPostsIndex = async () => await graphqlSdk.AllPostsIndex();
+export const allPostsArchive = async () => await graphqlSdk.AllPostsArchive();
+export const allPosts = async () => await graphqlSdk.AllPosts();
+export const postBySlug = async (slug: string) =>
+  await graphqlSdk.PostBySlug({ slug });
+export const postsByCategoryIdIndex = async (categoryId: number) =>
+  await graphqlSdk.PostsByCategoryIdIndex({ categoryId });
+export const postsByCategoryIdArchive = async (categoryId: number) =>
+  await graphqlSdk.PostsByCategoryIdArchive({ categoryId });
+export const postsByCategoryId = async (categoryId: number) =>
+  await graphqlSdk.PostsByCategoryId({ categoryId });
+export const postByAuthorSlugIndex = async (slug: string) =>
+  await graphqlSdk.PostByAuthorSlugIndex({ slug });
+export const postByAuthorSlugArchive = async (slug: string) =>
+  await graphqlSdk.PostByAuthorSlugArchive({ slug });
+export const postByAuthorSlug = async (slug: string) =>
+  await graphqlSdk.PostByAuthorSlug({ slug });
+export const postSEOBySlug = async (slug: string) =>
+  await graphqlSdk.PostSEOBySlug({ slug });
+export const postPerPage = async () => await graphqlSdk.PostPerPage();
+
+const POST_FIELDS = gql`
+  fragment PostFields on Post {
+    id
+    categories {
+      edges {
+        node {
+          databaseId
+          id
+          name
+          slug
+        }
+      }
+    }
+    databaseId
+    date
+    isSticky
+    postId
+    slug
+    title
+  }
+`;
+
+gql`
+  ${POST_FIELDS}
+  query AllPostsIndex {
+    posts(first: 10000, where: { hasPassword: false }) {
+      edges {
+        node {
+          ...PostFields
+        }
+      }
+    }
+  }
+`;
+
+gql`
+  ${POST_FIELDS}
+  query AllPostsArchive {
+    posts(first: 10000, where: { hasPassword: false }) {
+      edges {
+        node {
+          ...PostFields
+          author {
+            node {
+              avatar {
+                height
+                url
+                width
+              }
+              id
+              name
+              slug
+            }
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`;
+
+gql`
+  ${POST_FIELDS}
+  query AllPosts {
+    posts(first: 10000, where: { hasPassword: false }) {
+      edges {
+        node {
+          ...PostFields
+          author {
+            node {
+              avatar {
+                height
+                url
+                width
+              }
+              id
+              name
+              slug
+            }
+          }
+          content
+          excerpt
+          featuredImage {
+            node {
+              altText
+              caption
+              sourceUrl
+              srcSet
+              sizes
+              id
+            }
+          }
+          modified
+        }
+      }
+    }
+  }
+`;
+
+gql`
+  query PostBySlug($slug: ID!) {
+    post(id: $slug, idType: SLUG) {
+      author {
+        node {
+          avatar {
+            height
+            url
+            width
+          }
+          id
+          name
+          slug
+        }
+      }
+      id
+      categories {
+        edges {
+          node {
+            databaseId
+            id
+            name
+            slug
+          }
+        }
+      }
+      content
+      date
+      excerpt
+      featuredImage {
+        node {
+          altText
+          caption
+          sourceUrl
+          srcSet
+          sizes
+          id
+        }
+      }
+      modified
+      databaseId
+      title
+      slug
+      isSticky
+    }
+  }
+`;
+
+gql`
+  ${POST_FIELDS}
+  query PostsByCategoryIdIndex($categoryId: Int!) {
+    posts(where: { categoryId: $categoryId, hasPassword: false }) {
+      edges {
+        node {
+          ...PostFields
+        }
+      }
+    }
+  }
+`;
+
+gql`
+  ${POST_FIELDS}
+  query PostsByCategoryIdArchive($categoryId: Int!) {
+    posts(where: { categoryId: $categoryId, hasPassword: false }) {
+      edges {
+        node {
+          ...PostFields
+          author {
+            node {
+              avatar {
+                height
+                url
+                width
+              }
+              id
+              name
+              slug
+            }
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`;
+
+gql`
+  ${POST_FIELDS}
+  query PostsByCategoryId($categoryId: Int!) {
+    posts(where: { categoryId: $categoryId, hasPassword: false }) {
+      edges {
+        node {
+          ...PostFields
+          author {
+            node {
+              avatar {
+                height
+                url
+                width
+              }
+              id
+              name
+              slug
+            }
+          }
+          content
+          excerpt
+          featuredImage {
+            node {
+              altText
+              caption
+              id
+              sizes
+              sourceUrl
+              srcSet
+            }
+          }
+          modified
+        }
+      }
+    }
+  }
+`;
+
+gql`
+  ${POST_FIELDS}
+  query PostByAuthorSlugIndex($slug: String!) {
+    posts(where: { authorName: $slug, hasPassword: false }) {
+      edges {
+        node {
+          ...PostFields
+        }
+      }
+    }
+  }
+`;
+
+gql`
+  ${POST_FIELDS}
+  query PostByAuthorSlugArchive($slug: String!) {
+    posts(where: { authorName: $slug, hasPassword: false }) {
+      edges {
+        node {
+          ...PostFields
+          excerpt
+        }
+      }
+    }
+  }
+`;
+
+gql`
+  ${POST_FIELDS}
+  query PostByAuthorSlug($slug: String!) {
+    posts(where: { authorName: $slug, hasPassword: false }) {
+      edges {
+        node {
+          ...PostFields
+          excerpt
+          featuredImage {
+            node {
+              altText
+              caption
+              id
+              sizes
+              sourceUrl
+              srcSet
+            }
+          }
+          modified
+        }
+      }
+    }
+  }
+`;
+
+gql`
+  query PostSEOBySlug($slug: ID!) {
+    post(id: $slug, idType: SLUG) {
+      id
+      seo {
+        canonical
+        metaDesc
+        metaRobotsNofollow
+        metaRobotsNoindex
+        opengraphAuthor
+        opengraphDescription
+        opengraphModifiedTime
+        opengraphPublishedTime
+        opengraphPublisher
+        opengraphTitle
+        opengraphType
+        readingTime
+        title
+        twitterDescription
+        twitterTitle
+        twitterImage {
+          altText
+          sourceUrl
+          mediaDetails {
+            width
+            height
+          }
+        }
+        opengraphImage {
+          altText
+          sourceUrl
+          mediaDetails {
+            height
+            width
+          }
+        }
+      }
+    }
+  }
+`;
+
+gql`
+  query PostPerPage {
+    allSettings {
+      readingSettingsPostsPerPage
+    }
+  }
+`;
