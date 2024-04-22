0fdedb132cc3611110047cfd746babe8e434efde
gioboa <giorgiob.boa@gmail.com>
2024-04-22 22:51:53 +0200

    feat: change Nav component

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,14 +5,14 @@ import { Qwik } from "./Icons/Qwik";
 export const Nav = component$(() => {
   return (
     <nav class="whitespace-nowrap bg-white px-6 py-4 text-xl font-semibold text-gray-800 shadow ">
-      <div class="container mx-auto flex flex-row justify-between">
+      <div class="flex flex-row justify-between">
         <div class="flex items-center justify-between">
           <a href="/" class="ms-2 flex hover:text-blue-500 md:me-24">
             <Qwik />
             <span class="self-center">Qwik WordPress Starter</span>
           </a>
         </div>
-        <a href="/" class="hidden hover:text-blue-500 md:block">
+        <a href="/" class="mr-14 hidden hover:text-blue-500 md:block">
           <span>About Us</span>
         </a>
       </div>
